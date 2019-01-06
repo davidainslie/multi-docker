a9540f72d2145022be876f4b05872e189e33789b
David Ainslie <dainslie@gmail.com>
2019-01-06 19:47:23 +0000

    Some errors in Kubernetes related to promises???

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -35,9 +35,10 @@ class Fib extends Component {
   };
 
   renderSeenIndexes() {
-    if (this.state.seenIndexes.length > 0) {
+    try {
       return this.state.seenIndexes.map(({ number }) => number).join(', ');
-    } else {
+    }
+    catch(err) {
       return "";
     }
   }
